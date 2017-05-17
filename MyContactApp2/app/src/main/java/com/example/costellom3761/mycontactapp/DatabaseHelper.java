2a30537d761c1b4dbf4b1ea13340ed4e5644740e
megancostello <CostelloM3761@sduhsd.lan>
2017-05-17 09:26:19 -0700

    Added Age and Address text fields, toast messages, and viewAllData button and dialog box
    Tested on phone, viewData button successfully works. Shows ID, Name,
    Age, and Address

--- MyContactApp2/app/src/main/java/com/example/costellom3761/mycontactapp/DatabaseHelper.java
+++ MyContactApp2/app/src/main/java/com/example/costellom3761/mycontactapp/DatabaseHelper.java
@@ -15,15 +15,19 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "Contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "AGE";
+    public static final String COL_4 = "ADDRESS";
 
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, 3);
     }
+    //1 is for number of databases
+    //version 2 database is better for more columns in a table
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT, ADDRESS TEXT)");
     }
 
     @Override
@@ -32,12 +36,15 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         onCreate(db);
     }
 
-    public boolean insertData(String name) {
+    public boolean insertData(String name, String age, String address) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, name);
+        contentValues.put(COL_3, age);
+        contentValues.put(COL_4, address);
 
         long result = db.insert(TABLE_NAME, null, contentValues);
+
         if(result == -1) return false;
         else return true;
     }
--- MyContactApp2/app/src/main/java/com/example/costellom3761/mycontactapp/MainActivity.java
+++ MyContactApp2/app/src/main/java/com/example/costellom3761/mycontactapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.costellom3761.mycontactapp;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
-    EditText editName;
+    EditText editName, editAge, editAddress;
     Button btnAddData;
 
     @Override
@@ -22,24 +25,68 @@ public class MainActivity extends AppCompatActivity {
 
         //add the layout vars
         editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
     }
 
     public void addData(View v) {
-        boolean isInserted = myDb.insertData(editName.getText().toString());
+        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                editAge.getText().toString(), editAddress.getText().toString());
 
         if(isInserted == true) {
             Log.d("MyContact", "Success inserting data");
             //insert toast message here...
+            Toast.makeText(getApplicationContext(), "SUCCESS",
+                    Toast.LENGTH_SHORT).show();
         }
         else {
             Log.d("MyContact", "Failure inserting data");
             //insert toast message here...
+            Toast.makeText(getApplicationContext(), "FAILURE",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
-   // public void viewData(View v) {
-     //   Cursor res = myDb.getAllData();
+    public void viewData(View v) {
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
 
-    //}
+            //output message using log.d and toast
+            Log.d("MyContact", "Unable to show data because there is none");
+            Toast.makeText(getApplicationContext(), "Cannot viewData, no data in database",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //setup a loop with cursor(res) using moveToNext
+        //append each Col to the buffer
+        //display message using showMessage
+
+
+        while (res.moveToNext()) {
+            String idString = res.getString(0);
+            String nameString = res.getString(1);
+            String ageString = res.getString(2);
+            String addressString = res.getString(3);
+
+            buffer.append("ID: " + idString + "\n" + "name: " + nameString + "\n" + "age: " + ageString + "\n" +
+                "address: " + addressString + "\n\n");
+
+        }
+
+        showMessage("Data", buffer.toString());
+
+    }
+
+    private void showMessage(String title, String message) {
+        //alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true); //cancel using back button
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
 
 }
